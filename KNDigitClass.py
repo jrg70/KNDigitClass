3e83b1dab945d48fe331f3feb7b5551b0e836942
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-06-22 09:19:29 +0100

    Update KNDigitClass.py

--- KNDigitClass.py
+++ KNDigitClass.py
@@ -13,9 +13,9 @@ labels = pd.Series(digits['target'])
 data = pd.DataFrame(digits['data'])
 
 #values we re-use numerous times
-k_values = np.arange(1,21,1) #used in knn models
-n_splits = np.arange(4,13,2) #used in k-fold cross validation
-neurons = [pow(2, x) for x in np.arange(2,9,1)] #used in nn models
+k_values = np.arange(1,21,1) 
+n_splits = np.arange(4,13,2) 
+neurons = [pow(2, x) for x in np.arange(2,9,1)] 
 
 #lists to hold resulting accuracy lists
 model_results = []
@@ -38,36 +38,29 @@ def cross_validate(k, n_splits=4):
     test_acc = []
     kf = KFold(n_splits, shuffle=True, random_state=1)
     for train_index, test_index in kf.split(data):
-        #splitting data
+        
         train_features, test_features = data.loc[train_index], data.loc[test_index]
         train_labels, test_labels = labels.loc[train_index], labels.loc[test_index]
-        
-        #train model
+       
         model = train_knn(k, train_features, train_labels)
-        
-        #calculate accuracies
+       
         train_accuracy = test(model, train_features, train_labels)
         test_accuracy = test(model, test_features, test_labels)
         
-        #append accuracies to list
         train_acc.append(train_accuracy)
         test_acc.append(test_accuracy)
         
     return np.mean(train_acc), np.mean(test_acc)
   
-#function to help resuse code
+
 def plot_knn_model(title, n_splits=4):
-    #lists to contain accuracies of respective training/test sets
     fold_train_acc = {}
     fold_test_acc = {}
     train_acc = []
     test_acc = []
-    
-    #figure details
     fig, ax = plt.subplots(figsize=(12,8))
     fig.tight_layout(pad=3)
     fig.suptitle(y=0.9, t=f'Mean Accuracy vs. {title}', va='bottom', fontsize=22)
-    
     if type(n_splits) != int:
         #loop to calculate accuracies with various k & n values
         for k in k_values:
@@ -85,7 +78,7 @@ def plot_knn_model(title, n_splits=4):
             train_acc.append(training)
             test_acc.append(testing)
 
-    #plot
+    
     if type(n_splits) != int:
         ax.set_xticks(n_splits, fontsize=14)
         ax.set_xlabel('Number of Folds', fontsize=14)
